89233050fb07f18d828a9ddd5f68a52e6148a451
fabriceandi <tojofenitraandriah@gmail.com>
2024-06-21 09:25:49 +0200

    Create files

---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,20 @@
+public class Journal
+{
+    public List<Entry> _entries;
+    public void AddEntry(Entry newEntry)
+    {
+
+    }
+    public void DisplayALL()
+    {
+
+    }   
+    public void SaveToFile(string file)
+    {
+
+    }
+    public void LoadFromFile(string file)
+    {
+
+    }
+}
