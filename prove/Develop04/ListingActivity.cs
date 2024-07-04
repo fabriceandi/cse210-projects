fd06165d80e31d1dc610eb1127f8a9254162186c
fabriceandi <tojofenitraandriah@gmail.com>
2024-07-04 21:32:26 +0200

    Mindfulness Program

---
+++ prove/Develop04/ListingActivity.cs
@@ -0,0 +1,25 @@
+using System;
+
+public class ListingActivity : Activity
+{
+    private int _count;
+    private List<string> _prompts;
+
+    public ListingActivity()
+    {
+
+    }
+    public void Run()
+    {
+
+    }
+    public void GetRandomPrompt()
+    {
+
+    }
+    public List<string> GetListFromUser()
+    {
+        List<string> list = new List<string>();
+        return list;
+    }
+}
