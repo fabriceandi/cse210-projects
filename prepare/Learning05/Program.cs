9aa86b4ef4f0a2bf0fa1cdc59b69c0d033d39c45
fabriceandi <tojofenitraandriah@gmail.com>
2024-07-09 23:04:31 +0200

    Shapes

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square("blue", 4);
+        //Console.WriteLine(square.GetColor());
+        //Console.WriteLine(square.GetArea());
+
+        Rectangle rectangle = new Rectangle("yellow", 6, 4);
+        //Console.WriteLine(rectangle.GetColor());
+        //Console.WriteLine(rectangle.GetArea());
+
+        Circle circle = new Circle("red", 5);
+        //Console.WriteLine(circle.GetColor());
+        //Console.WriteLine(circle.GetArea());
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach(Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
     }
 }
